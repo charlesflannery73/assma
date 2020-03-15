b01a657f6e7a24a1eb328f2ec32906673c1c8ddf
Charles Flannery <charlesflannery73@gmail.com>
2020-03-15 11:49:41 +0400

    add validators to clean function in Asset model removed mask6, renamed nask4 to netmask4

--- web/admin.py
+++ web/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Org, Asset, Sector, Level
+from .models import Org, Asset, Sector, Level, AssetType
 
 admin.site.register(Sector)
 admin.site.register(Level)
 admin.site.register(Org)
 admin.site.register(Asset)
+admin.site.register(AssetType)
--- web/models.py
+++ web/models.py
@@ -3,6 +3,8 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import User
 from django.db import models
 import ipaddress
+import re
+import validators
 
 class Sector(models.Model):
     sector = models.CharField(max_length=255, null=False)
@@ -61,16 +63,78 @@ class Asset(models.Model):
 
     def clean(self):
         super(Asset, self).clean()
+
         if str(self.type) == "ipv4":
             try:
-                ipaddress.IPv4Address(str(self.name));
+                ipaddress.IPv4Address(str(self.name))
             except ValueError:
                 raise ValidationError('\"' + self.name + '\" is not a valid ipv4 address')
+
         if str(self.type) == "ipv6":
             try:
-                ipaddress.IPv6Address(str(self.name));
+                ipaddress.IPv6Address(str(self.name))
             except ValueError:
                 raise ValidationError('\"' + self.name + '\" is not a valid ipv6 address')
 
+        if str(self.type) == "cidr4":
+            try:
+                ipaddress.IPv4Network(str(self.name))
+            except ValueError:
+                raise ValidationError('\"' + self.name + '\" is not a valid ipv4 cidr address')
+
+        if str(self.type) == "cidr6":
+            try:
+                ipaddress.IPv6Network(str(self.name))
+            except ValueError:
+                raise ValidationError('\"' + self.name + '\" is not a valid ipv6 cidr address')
+
+        if str(self.type) == "range4":
+            match = re.search('[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}-[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}', self.name,  )
+            if match:
+                pair = self.name.split("-")
+                test = ipaddress.IPv4Address(pair[1]) > ipaddress.IPv4Address(pair[0])
+                if test == False:
+                    raise ValidationError('\"' + pair[1] + '\" is not higher than \"' + pair[0] + '\" try again!')
+
+                for ip4 in pair:
+                    try:
+                        ipaddress.IPv4Address(str(ip4))
+                    except ValueError:
+                        raise ValidationError('\"' + ip4 + '\" is not a valid ipv4 address')
+            else:
+                raise ValidationError('\"' + self.name + '\" is not a valid range4 address, please use a hyphen as a separator eg 1.2.3.4-1.2.3.8')
+
+        if str(self.type) == "range6":
+            match = re.search('[0-9a-f:]+-[0-9a-f:]+', self.name, re.I)
+            if match:
+                pair = self.name.split("-")
+                test = ipaddress.IPv6Address(pair[1]) > ipaddress.IPv6Address(pair[0])
+                if test == False:
+                    raise ValidationError('\"' + pair[1] + '\" is not higher than \"' + pair[0] + '\" try again!')
+                for ip6 in pair:
+                    try:
+                        ipaddress.IPv6Address(str(ip6))
+                    except ValueError:
+                        raise ValidationError('\"' + ip6 + '\" is not a valid ipv6 address')
+            else:
+                raise ValidationError('\"' + self.name + '\" is not a valid range6 address, please use a hyphen as a separator eg 2001:db8::-2001:db8:0000:0000:0000:0000:0000:00ff')
+
+        if str(self.type) == "mask4":
+            match = re.search('/', self.name,  )
+            if match:
+                try:
+                    ipaddress.IPv4Network(str(self.name))
+                except ValueError:
+                    raise ValidationError('\"' + self.name + '\" is not a valid ipv4 netmask, please use forward slash as separator eg 192.168.10.0/255.255.255.0')
+            else:
+                raise ValidationError('\"' + self.name + '\" is not a valid ipv4 netmask, please use forward slash as separator eg 192.168.10.0/255.255.255.0')
+
+        if str(self.type) == "domain":
+            test = validators.domain(self.name)
+            if test != True:
+                raise ValidationError('\"' + self.name + '\" is not a valid domain name')
+
+
     # def get_absolute_url(self):
     #     return reverse('asset-detail', kwargs={'pk': self.pk})
+
