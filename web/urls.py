e139784e10fd7d0bacb7daa407750c47b43732c8
Charles Flannery <charlesflannery73@gmail.com>
2020-03-15 16:10:46 +0400

    started whois function

--- web/urls.py
+++ web/urls.py
@@ -32,6 +32,8 @@ urlpatterns = [
     path('asset/new/', AssetCreateView.as_view(), name='asset-create'),
     path('asset/<int:pk>/update/', AssetUpdateView.as_view(), name='asset-update'),
     path('asset/<int:pk>/delete/', AssetDeleteView.as_view(), name='asset-delete'),
+    path('asset/<int:pk>/whois/', views.whois_local, name='asset-whois'),
+    path('asset/<int:pk>/nslookup/', views.nslookup, name='asset-nslookup'),
     path('about/', views.about, name='assma-about'),
 ]
 
